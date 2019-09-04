0c2f5bfaac7239c6a850be5cfd8725230c93fce4
myles-dewaal <mylesdewaal@hotmail.com>
2019-09-03 20:11:30 -0600

    adding to foundation

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,21 @@
-alert ("good luck")
+//Variables
+var won = 0;
+var lost = 0;
+var attempts = 10;
+var usedArray = [];
+var ranLetter = ranLetter;
+var letters = "abcdefghijklmnopqrstuvwxyz"
+
+//Generating the random letter
+ranLetter = letters[Math.floor(Math.random() * letters.length)];
+console.log(ranLetter);
+function jsGuess() {
+    ranLetter = letters[Math.floor(Math.random() * letters.length)];
+    console.log(ranLetter);
+
+}
+
+//Players input
+document.onkeyup = function (event) {
+    var playerGuess = event.key;
+
